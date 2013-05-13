6ec9d1f535ee45a7e88b23a521d47fb3ae962065
Junior Cissokho--Papin <junior.cissokhopapin@gmail.com>
2013-05-13 14:22:34 +0200

    Cryptage des mots de passe

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -55,7 +55,7 @@ class PdoGsb{
 */
 	public function getInfosVisiteur($login, $mdp){
 		$req = "select visiteur.id as id, visiteur.nom as nom, visiteur.prenom as prenom from visiteur 
-		where visiteur.login='$login' and visiteur.mdp='$mdp'";
+		where visiteur.login='$login' and visiteur.mdp=PASSWORD('$mdp')";
 		$rs = PdoGsb::$monPdo->query($req);
 		$ligne = $rs->fetch();
 		return $ligne;
